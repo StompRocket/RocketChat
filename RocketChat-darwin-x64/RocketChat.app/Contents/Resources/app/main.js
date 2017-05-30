787f89bfb39c0a65d20dd51170fd5c13d0e4ec40
Rosho123 <rsf.sho@gmail.com>
2017-05-29 17:20:38 -0700

    built new version

--- RocketChat-darwin-x64/RocketChat.app/Contents/Resources/app/main.js
+++ RocketChat-darwin-x64/RocketChat.app/Contents/Resources/app/main.js
@@ -5,17 +5,34 @@ const BrowserWindow = electron.BrowserWindow
 const Menu = electron.Menu
 const path = require('path')
 const url = require('url')
-let mainWindow
+let mainWindow, looadingwindow
 
 function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 800
+  const {
+    BrowserWindow
+  } = require('electron')
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: false
   })
-  mainWindow.loadURL('http://localhost:3007');
-  mainWindow.on('closed', function() {
-    mainWindow = null
-  })
+  modalPath = path.join('file://', __dirname, '/startpage.html')
+  win.loadURL(modalPath);
+  setTimeout(
+  function()
+  {
+    win = null;
+    mainWindow = new BrowserWindow({
+      width: 1200,
+      height: 800
+    })
+    mainWindow.loadURL('http://localhost:3007');
+    mainWindow.on('closed', function() {
+      mainWindow = null
+    })
+  }, 5000);
+
+
 }
 app.on('ready', createWindow)
 app.on('window-all-closed', function() {
